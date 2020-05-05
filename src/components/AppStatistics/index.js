133817b455bf4421feb7285bba32270341383714
Bilal Irmak <bilal.irmak00@gmail.com>
2020-05-05 18:13:10 +0300

    ADDED different cities count

--- src/components/AppStatistics/index.js
+++ src/components/AppStatistics/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Statistic, Card, Spin } from 'antd';
+import {Statistic, Card, Spin} from 'antd';
 import {inject, observer} from "mobx-react";
 
 @inject('SocketStore')
@@ -12,65 +12,76 @@ class AppStatistics extends Component {
         };
 
         return (
-            SocketStore.data === null ? <Spin/>:
+            SocketStore.data === null ? <Spin/> :
                 <div style={{
-                display: "flex",
-                justifyContent: 'center',
-                margin: 20,
-            }}>
-                <Card>
-                    <div style={statDiv}>
-                        <Statistic
-                            style={{
-                                textAlign: "center",
-                            }}
-                            title="Toplam araba sayısı" value={SocketStore.data[0].total_car_number}
+                    display: "flex",
+                    justifyContent: 'center',
+                    margin: 20,
+                }}>
+                    <Card>
+                        <div style={statDiv}>
+                            <Statistic
+                                style={{
+                                    textAlign: "center",
+                                }}
+                                title="Toplam araba sayısı" value={SocketStore.data[0].total_car_number}
 
-                        />
-                    </div>
-                </Card>
-                <Card>
-
-
-                    <div style={statDiv}>
-                        <Statistic
-                            style={{
-                                textAlign: "center",
-
-                            }}
-                            title="Farklı marka sayısı" value={SocketStore.data[0].files.length}
-
-                        />
-                    </div>
-                </Card>
-                <Card>
-
-                    <div style={statDiv}>
-                        <Statistic
-                            style={{
-                                textAlign: "center",
-
-                            }}
-                            title="Farklı model sayısı" value={new Set(SocketStore.data[0].models.flat()).size}
-
-                        />
-                    </div>
-                </Card>
-                <Card>
+                            />
+                        </div>
+                    </Card>
+                    <Card>
 
 
-                <div style={statDiv}>
-                    <Statistic
-                        style={{
-                            textAlign: "center",
+                        <div style={statDiv}>
+                            <Statistic
+                                style={{
+                                    textAlign: "center",
 
-                        }}
-                        title="Farklı renk sayısı" value={new Set(SocketStore.data[0].colors.flat()).size}
+                                }}
+                                title="Farklı marka sayısı" value={SocketStore.data[0].files.length}
 
-                    />
+                            />
+                        </div>
+                    </Card>
+                    <Card>
+
+                        <div style={statDiv}>
+                            <Statistic
+                                style={{
+                                    textAlign: "center",
+
+                                }}
+                                title="Farklı model sayısı" value={new Set(SocketStore.data[0].models.flat()).size}
+
+                            />
+                        </div>
+                    </Card>
+                    <Card>
+                        <div style={statDiv}>
+                            <Statistic
+                                style={{
+                                    textAlign: "center",
+
+                                }}
+                                title="Farklı renk sayısı" value={new Set(SocketStore.data[0].colors.flat()).size}
+
+                            />
+                        </div>
+                    </Card>
+                    <Card>
+                        <div style={statDiv}>
+                            <Statistic
+                                style={{
+                                    textAlign: "center",
+
+                                }}
+                                title="Farklı şehir sayısı"
+                                value={new Set(SocketStore.data[0].license_plates.flat()).size}
+
+                            />
+                        </div>
+                    </Card>
                 </div>
-                </Card>
-            </div>
 
         );
     }
